7de76f03de522531803a429de4a49220eb63700a
Carlos Costa <jr.carlosad@gmail.com>
2021-04-02 22:22:44 -0300

    feat: Preparação de métodos de agendamento

--- src/appointments/appointments.module.ts
+++ src/appointments/appointments.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { HttpModule, Module } from '@nestjs/common';
 import { AppointmentsService } from './appointments.service';
 import { AppointmentsController } from './appointments.controller';
 
 @Module({
+  imports: [HttpModule],
   controllers: [AppointmentsController],
   providers: [AppointmentsService],
 })
--- src/appointments/appointments.service.ts
+++ src/appointments/appointments.service.ts
@@ -1,26 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService, Injectable } from '@nestjs/common';
+import { map } from 'rxjs/operators';
+
 import { CreateAppointmentDto } from './dto/create-appointment.dto';
 import { UpdateAppointmentDto } from './dto/update-appointment.dto';
+import { BASE_EXTERNAL_API_URL } from 'src/utils/url';
 
 @Injectable()
 export class AppointmentsService {
+  constructor(private httpService: HttpService) {}
+
   create(createAppointmentDto: CreateAppointmentDto) {
-    return 'This action adds a new appointment';
+    return this.httpService
+      .post(`${BASE_EXTERNAL_API_URL}/appointments`, createAppointmentDto)
+      .pipe(map((response) => response.data));
   }
 
   findAll() {
-    return `This action returns all appointments`;
+    return this.httpService
+      .get(`${BASE_EXTERNAL_API_URL}/appointments`)
+      .pipe(map((response) => response.data));
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} appointment`;
+    return this.httpService
+      .get(`${BASE_EXTERNAL_API_URL}/appointments/${id}`)
+      .pipe(map((response) => response.data));
   }
 
   update(id: number, updateAppointmentDto: UpdateAppointmentDto) {
-    return `This action updates a #${id} appointment`;
+    return this.httpService
+      .patch(
+        `${BASE_EXTERNAL_API_URL}/appointments/${id}`,
+        updateAppointmentDto,
+      )
+      .pipe(map((response) => response.data));
   }
 
   remove(id: number) {
-    return `This action removes a #${id} appointment`;
+    return this.httpService
+      .delete(`${BASE_EXTERNAL_API_URL}/appointments/${id}`)
+      .pipe(map((response) => response.data));
   }
 }
