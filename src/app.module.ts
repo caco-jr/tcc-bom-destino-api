b6bdcfc442b1ea6525d26e965aa0dd7b3fdc6387
Carlos Costa <jr.carlosad@gmail.com>
2021-04-02 21:49:34 -0300

    feat: Criando endpoints necessárias

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthInstitutionsModule } from './health-institutions/health-institutions.module';
+import { UsersModule } from './users/users.module';
+import { AppointmentsModule } from './appointments/appointments.module';
 
 @Module({
-  imports: [HealthInstitutionsModule],
+  imports: [HealthInstitutionsModule, UsersModule, AppointmentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/utils/url.ts
@@ -0,0 +1,2 @@
+export const BASE_EXTERNAL_API_URL =
+  'https://tcc-bom-destino-mock.herokuapp.com';
